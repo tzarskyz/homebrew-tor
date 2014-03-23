6927c1eab8ed151deb1f6a7a3e54edd2aedc66a0
tzarskyz <tzarskyz@gmail.com>
2014-03-23 13:25:55 -0400

    Update tor.rb

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -2,62 +2,59 @@ require 'formula'
 
 class Tor < Formula
   homepage 'https://www.torproject.org/'
-  version "0.2.5.3-alpha"
-  url 'https://www.torproject.org/dist/tor-0.2.5.3-alpha.tar.gz'
-  sha256 '7dd9313a066e6d94131a07cf0a70ad1e8e3d92cbd752b1e1ca9baeb68a9bbd76'
-  head 'git://git.torproject.org/git/tor.git'
+  url 'https://www.torproject.org/dist/tor-0.2.4.21.tar.gz'
+  sha1 'b93b66e4d5162cefc711cb44f9167ed4799ef990'
 
-  option 'with-upnp', 'Enable upnp "PortForwarding" option.'
+  devel do
+    url 'https://www.torproject.org/dist/tor-0.2.5.3-alpha.tar.gz'
+    version '0.2.5.3-alpha'
+    sha1 '29784b3f711780cd60fff076f6deb9b1f633fe5c'
+  end
 
   depends_on 'libevent'
   depends_on 'openssl'
   depends_on 'miniupnpc' if build.include? 'with-upnp'
+  
+def install
+    # Fix the path to the control cookie.
+    inreplace \
+      'contrib/tor-ctrl.sh',
+      'TOR_COOKIE="/var/lib/tor/data/control_auth_cookie"',
+      'TOR_COOKIE="$HOME/.tor/control_auth_cookie"'
 
-  def install
-    configargs = %W[
-             --prefix=#{prefix}
-             --disable-dependency-tracking
-           ]
-    configargs << '--enable-upnp' if build.include? 'with-upnp'
 
-    system "./autogen.sh" if ARGV.build_head?
-    system "./configure", *configargs
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-openssl-dir=#{Formula["openssl"].opt_prefix}"
+                          "--enable-upnp"
     system "make install"
 
-    plist_path.write startup_plist
-    plist_path.chmod 0644
+    bin.install "contrib/tor-ctrl.sh" => "tor-ctrl"
   end
 
-  def startup_plist
-    return <<-EOPLIST
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>KeepAlive</key>
-    <true/>
-    <key>UserName</key>
-    <string>#{`whoami`.chomp}</string>
-    <key>ProgramArguments</key>
-    <array>
-        <string>#{HOMEBREW_PREFIX}/bin/tor</string>
-    </array>
-    <key>WorkingDirectory</key>
-    <string>#{HOMEBREW_PREFIX}</string>
-  </dict>
-</plist>
-    EOPLIST
+  test do
+    system "tor", "--version"
   end
 
-  def caveats; <<-EOS.undent
-    You can start tor automatically on login with:
-      mkdir -p ~/Library/LaunchAgents
-      cp #{plist_path} ~/Library/LaunchAgents/
-      launchctl load -w ~/Library/LaunchAgents/#{plist_path.basename}
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/tor</string>
+        </array>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+      </dict>
+    </plist>
     EOS
   end
 end
